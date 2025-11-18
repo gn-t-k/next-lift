0a033dc1f8cf48e860e13002e75e0631326284c9
gn-t-k <gntk1995422@gmail.com>
2025-11-18 15:59:28 +0900

    runtimeに設定していた環境変数をbuildに移動

--- packages/env/src/schemas/turso.ts
+++ packages/env/src/schemas/turso.ts
@@ -6,12 +6,12 @@ import type { Schemas } from "./type";
 const isProduction = process.env["NODE_ENV"] === "production";
 
 export const tursoEnvSchemas = {
-	privateBuild: z.object({}),
-	privateRuntime: z.object({
+	privateBuild: z.object({
 		TURSO_AUTH_DATABASE_URL: isProduction ? z.url() : z.string().optional(),
 		TURSO_AUTH_DATABASE_AUTH_TOKEN: isProduction
 			? z.string().min(1)
 			: z.string().min(1).optional(),
 	}),
+	privateRuntime: z.object({}),
 	publicRuntime: z.object({}),
 } as const satisfies Schemas;
