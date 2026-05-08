cfba74a96d20fa0b7ecc7fbb6e5050370abc4c80
gn-t-k <gntk1995422@gmail.com>
2026-05-08 11:33:56 +0900

    fix(react-components): main.tsx の format 整え（tab indent）

--- packages/react-components/src/primitives/main/main.tsx
+++ packages/react-components/src/primitives/main/main.tsx
@@ -7,22 +7,22 @@ import { HeadingContext } from "../heading/heading";
 type Props = VariantProps<typeof styles>;
 
 export const Main: FC<PropsWithChildren<Props>> = ({ width, children }) => {
-  return (
-    <HeadingContext value={1}>
-      <main className={styles({ width })}>{children}</main>
-    </HeadingContext>
-  );
+	return (
+		<HeadingContext value={1}>
+			<main className={styles({ width })}>{children}</main>
+		</HeadingContext>
+	);
 };
 
 const styles = tv({
-  base: "mx-auto w-full p-4",
-  variants: {
-    width: {
-      narrow: "max-w-2xl",
-      wide: "max-w-screen-xl",
-    },
-  },
-  defaultVariants: {
-    width: "narrow",
-  },
+	base: "mx-auto w-full p-4",
+	variants: {
+		width: {
+			narrow: "max-w-2xl",
+			wide: "max-w-screen-xl",
+		},
+	},
+	defaultVariants: {
+		width: "narrow",
+	},
 });
